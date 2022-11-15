87bbf1631ea7214c87ed57564d279552dbbfd780
Oscar Sanchez <sancheztornero.osc@gmail.com>
2022-11-15 17:18:57 +0100

    Root commit

---
+++ src/colores/Colores.java
@@ -0,0 +1,23 @@
+package colores;
+public class Colores {
+
+	public static void main(String[] args) {
+		//Codigo de colores
+		String Colores="\033[37m Colores: ";
+		String C = Colores;
+		String rojo="\033[31m"; 
+		String azul="\033[34m";
+		String verde="\033[32m";
+		String morado="\033[35m";
+		String cyan="\033[36m";
+		String amarillo ="\033[33m";
+		//Salida de colores
+		System.out.println(C+rojo+"Rojo");
+		System.out.println(C+azul+"Azul");
+		System.out.println(C+verde+"Verde");
+		System.out.println(C+morado+"Morado");
+		System.out.println(C+cyan+"Cyan");
+		System.out.println(C+amarillo+"Amarillo");
+	}
+
+}
